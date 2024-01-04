77e7b6bc846a682fc771ece17952a72164e5d5ab
Satwik Gupta <satwikgupta1801@gmail.com>
2024-01-05 00:20:54 +0530

    nav bar

---
+++ script1.js
@@ -0,0 +1,7 @@
+document.getElementById("header-menu-trigger").addEventListener("click", () => {
+    document.getElementById("menu-nav-wrap").classList.toggle("active")
+})
+
+document.getElementById("close-button").addEventListener("click", () => {
+    document.getElementById("menu-nav-wrap").classList.toggle("active")
+})
